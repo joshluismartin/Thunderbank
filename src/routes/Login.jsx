7dbf3dd6e61e50e9e309a316bddb69705682bd8c
joshluismartin <joshluis.martin>
2024-03-15 17:42:31 +0800

    style: added few styles to Loging Page

--- src/routes/Login.jsx
+++ src/routes/Login.jsx
@@ -1,14 +1,28 @@
-import React, { useState } from 'react';
-
+import React, { useEffect, useState } from 'react';
+import { Input, Heading, } from '@chakra-ui/react'
+import { Button } from '@chakra-ui/react'
+import {
+  Alert,
+  AlertIcon,
+  AlertTitle,
+  AlertDescription,
+} from '@chakra-ui/react'
 
 const Login = () => {
+
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState('');
 
   const handleLogin = () => {
     if (username !== 'useradmin' || password !== 'stanleyispogi') {
-      setError('Invalid username or password');
+      setError(
+        <Alert status='error'>
+          <AlertIcon />
+          <AlertTitle>Invalid Credentials!</AlertTitle>
+          <AlertDescription>Please Check Your Username or Password</AlertDescription>
+        </Alert>
+      );
     } else {
       localStorage.setItem('loggedIn', 'true');
       console.log('Login successful');
@@ -17,22 +31,26 @@ const Login = () => {
 
 
   return (
-    <div className="login-container">
-      <h2>Login to Your Banking Account</h2>
-      {error && <p className="error">{error}</p>}
-      <input
-        type="text"
-        placeholder="Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button onClick={handleLogin}>Login</button>
+    <div>
+      <div className="login-container">
+        <Heading mb={4} color='#D69E2E'>ThunderBank Login</Heading>
+        {error && <p className="error">{error}</p>}
+        <Input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <Input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <Button colorScheme='teal' variant='ghost' onClick={handleLogin}>
+          Login
+        </Button>
+      </div>
     </div>
   );
 };
