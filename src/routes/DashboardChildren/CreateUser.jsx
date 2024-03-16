608dc9192978b9c99244148286c557a9ac26b0ae
redroboto <vince.pleyto@gmail.com>
2024-03-16 13:23:55 +0800

    implemented react hook form instead of hardcoding functions to capture data from input fields. The userdata is now saved to local storage. Added birthday and user id to the user as well.

--- src/routes/DashboardChildren/CreateUser.jsx
+++ src/routes/DashboardChildren/CreateUser.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
 import { useState, useRef } from "react";
-import { UseForm } from "react-hook-form";
+import { useForm } from "react-hook-form";
 
 export default function CreateUser() {
   const {
@@ -10,25 +10,34 @@ export default function CreateUser() {
     handleSubmit,
     watch,
     formState: { errors },
+    reset,
   } = useForm();
 
   // set initial structure and values of userInfo
-  const [userInfo, setUserInfo] = useState({
-    user: "",
-    balance: "",
-    userId: "",
-  });
+  // const [userInfo, setUserInfo] = useState({
+  //   user: "",
+  //   balance: "",
+  //   userId: "",
+  // });
   const userDatabase = useRef([]);
 
+  let uuid = self.crypto.randomUUID();
+
+  console.log(uuid);
+
   // func to capture user name and balance from the input elements
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setUserInfo((prevUserInfo) => ({ ...prevUserInfo, [name]: value }));
-  };
+  // const handleChange = (e) => {
+  //   const { name, value } = e.target;
+  //   setUserInfo((prevUserInfo) => ({ ...prevUserInfo, [name]: value }));
+  // };
 
   //   func to store the current userInfo in the array upon clicking submit
   const onSubmit = (data) => {
     console.log(data);
+    userDatabase.current.push(data);
+    localStorage.setItem("users", JSON.stringify(userDatabase.current));
+
+    reset();
     // e.preventDefault();
     // // stores current userInfo inside the useRef
     // userDatabase.current.push(userInfo);
@@ -43,26 +52,35 @@ export default function CreateUser() {
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)}>
-        <label htmlFor="">Input New User's Name</label>
+        <label>First Name: </label>
         <input
-          type="text"
-          name="user"
-          value={userInfo.user}
-          onChange={handleChange}
+          {...register("First Name", { required: true })}
+          title="Use upper or lower case letters only"
+          pattern="[A-Za-z\s\-']+"
+        />
+
+        <label>Last Name: </label>
+        <input
+          {...register("Last Name")}
           title="Use upper or lower case letters only"
           pattern="[A-Za-z\s\-']+"
           required
         />
 
+        <label htmlFor="">Birthday: </label>
+        <input type="date" {...register("Birth Date")} />
+
         <label htmlFor="">Input Initial Balance</label>
         <input
           type="number"
           name="balance"
-          value={userInfo.balance}
-          onChange={handleChange}
+          {...register("Balance", { required: true })}
           min="0"
           required
         />
+
+        <label htmlFor="">User ID</label>
+        <input type="text" defaultValue={uuid} {...register("User ID")} />
 
         <input type="submit" value="Submit" />
       </form>
