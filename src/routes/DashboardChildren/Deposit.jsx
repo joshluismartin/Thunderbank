d6d1fd5184aa0cd52d4a5b5da4b7d0ad390abb29
Ella Repre <roselarepre@yahoo.com>
2024-03-16 16:51:33 +0800

    add changes to deposit modal

--- src/routes/DashboardChildren/Deposit.jsx
+++ src/routes/DashboardChildren/Deposit.jsx
@@ -1,24 +1,60 @@
 import { useState, useEffect } from "react"
 import './styles.css'
+import '../../src/routes/css/Deposit.css'
+import { useDisclosure, Modal, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter, Button, ModalOverlay } from '@chakra-ui/react'
+import { TableContainer, Table, TableCaption, Thead, Th, Tbody, Tr, Td } from '@chakra-ui/react'
 
-export function Deposit() {
-  const [balance, setBalance] = useState(1000);
+export default function Deposit() {
+  const [balance, setBalance] = useState(0);
   const [amount, setAmount] = useState(0)
+  const { isOpen, onOpen, onClose } = useDisclosure()
+  const [error, setError] = useState('')
+  const [notice, setNotice] = useState('')
+  const [transactions, setTransactions] = useState([])
 
   const deposit = () => {
+    setError('')
+    setNotice('')
     if (Number(amount) > 0) {
       setBalance(prev => Number(prev) + Number(amount))
+      setNotice(`Successfully deposited ${formattedAmount(amount)}`)
+      setTransactions([...transactions,{
+        amount: amount,
+        status: 'deposit',
+        balance: balance + Number(amount),
+        timedate: 'March 16,2024'
+      }])
     } else {
+      setError('Invalid deposit amount')
       console.log('Invalid deposit amount')
     }
   }
 
   const withdraw = () => {
+    setError('')
+    setNotice('')
+
     if (Number(amount) > 0 && Number(amount) <= balance){
       setBalance(prev => Number(prev) - Number(amount));
+      setNotice(`Successfully withdrawn ${formattedAmount(amount)}`)
+      setTransactions([...transactions,{
+        amount: amount,
+        status: 'withdraw',
+        balance: balance - Number(amount),
+        timedate: 'March 16,2024'
+      }])
     } else {
-    console.log('Invalid withdrawal amount');
+      setError('Invalid withdrawal amount')
+      console.log('Invalid withdrawal amount');
     }
+  }
+
+  const formattedAmount = (amount) => {
+    return new Intl.NumberFormat('en-PH', {
+      style: 'currency',
+      currency: 'PHP',
+      minimumFractionDigits: 2,
+    }).format(amount)
   }
 
   const formattedBalance = new Intl.NumberFormat('en-PH', {
@@ -27,13 +63,67 @@ export function Deposit() {
     minimumFractionDigits: 2,
   }).format(balance);
 
+  const onClickModal = () => {
+    onOpen()
+  }
+  
   return (
-    <div className="balance-container">
-      <p className="balanceAmount">Balance</p>
-      <div>{formattedBalance}</div>
-      <input type="number" onChange={(e) => setAmount(e.target.value)}></input>
-      <button onClick={deposit}>Deposit</button>
-      <button onClick={withdraw}>Withdraw</button>
-    </div>
+    <>
+      <Button
+        onClick={onClickModal}
+      >
+        Transactions
+      </Button>
+      <Modal isCentered isOpen={isOpen} onClose={onClose}>
+          <ModalOverlay
+          bg='blackAlpha.300'
+          backdropFilter='blur(0.5px) hue-rotate(90deg)'
+        />
+        <ModalContent>
+          <ModalHeader>Transactions</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <div className="balance-container">
+              <p className="balanceAmount">Josh Luis Martin</p>
+              <div>{formattedBalance}</div>
+              <div className="error">{error}</div>
+              <div className="notice">{notice}</div>
+              <input className="number-input" type="number" onChange={(e) => setAmount(e.target.value)} placeholder="Please enter amount"></input>
+              <button onClick={deposit} className="transaction-button">Deposit</button>
+              <button onClick={withdraw} className="transaction-button">Withdraw</button>
+            </div>
+            <TableContainer>
+              <Table size='sm' variant='striped' colorScheme='teal'>
+                <Thead>
+                  <Tr>
+                    <Th>Time & Date</Th>
+                    <Th>Status</Th>
+                    <Th>Amount</Th>
+                    <Th isNumeric>Balance</Th>
+                  </Tr>
+                </Thead>
+                <Tbody>
+                  {transactions.map((transaction, index) => (
+                    <Tr>
+                    <Td>{transaction.timedate}</Td>
+                    <Td>{transaction.status}</Td>
+                    <Td>{transaction.amount}</Td>
+                    <Td>{transaction.balance}</Td>
+                  </Tr>
+                  ))}
+
+                </Tbody>
+              </Table>
+            </TableContainer>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose}>Close</Button>
+          </ModalFooter>
+        </ModalContent>
+        
+      </Modal>
+      
+    </>
+    
   );
 }
--- src/routes/DashboardChildren/Transactions.jsx
+++ src/routes/DashboardChildren/Transactions.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { Outlet } from "react-router-dom";
+import Deposit from '../../../components/Deposit/Deposit.jsx'
 
 export default function Transactions() {
-  return (<div> Transactions </div>)
+  return (
+    <>
+      <div> Transactions </div>
+      <Deposit />
+    </>
+  )
 }
