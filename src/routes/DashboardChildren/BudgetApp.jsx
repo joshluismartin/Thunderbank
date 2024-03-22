a902b5e788237efeae677dc7ded1ace83923abe5
joshluismartin <joshluis.martin>
2024-03-22 20:03:40 +0800

    working on BudgetApp

--- src/routes/DashboardChildren/BudgetApp.jsx
+++ src/routes/DashboardChildren/BudgetApp.jsx
@@ -7,8 +7,10 @@ import {
   Input,
   Button,
   extendTheme,
+  Select,
 } from '@chakra-ui/react';
 import { User } from './User.js'; // Assume User class is exported from User.js
+
 
 // Custom theme to extend Chakra UI
 const theme = extendTheme({
@@ -22,6 +24,16 @@ const theme = extendTheme({
 });
 
 function BudgetApp() {
+
+  let users = JSON.parse(localStorage.getItem("users")) || [];
+  const [stanuSers, setStanusers] = useState(JSON.parse(localStorage.getItem("users") || "[]"))
+  const [senderId, setSenderId] = useState('')
+
+  let sender = users.find(user =>
+    user.userId === senderId);
+  console.log(sender)
+  /*let recipient = users.find(user =>
+    user.userId === recipientId);*/
   const [user, setUser] = useState(new User('user@example.com', 'password123', 'Jane Doe', 1000));
   const [name, setName] = useState('');
   const [cost, setCost] = useState('');
@@ -34,9 +46,15 @@ function BudgetApp() {
 
   return (
     <ChakraProvider theme={theme}>
+      <label> User </label>
+      <Select onChange={value => setSenderId(value.target.value)}>
+        {stanuSers.map(user => (
+          <option value={user.userId}>{user.firstName} {user.lastName}</option>))}
+      </Select>
+
       <Box p={5}>
         <VStack spacing={4}>
-          <Text>Current Balance: {user.accountBalance}</Text>
+         /* <Text>Current Balance: {user.balance}</Text>*/
           <Input placeholder="Expense Name" value={name} onChange={(e) => setName(e.target.value)} />
           <Input placeholder="Cost" type="number" value={cost} onChange={(e) => setCost(e.target.value)} />
           <Button onClick={handleAddExpense}>Add Expense</Button>
